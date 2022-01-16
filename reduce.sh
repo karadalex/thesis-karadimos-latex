8ded136d9765f61a2c1d068f08ad568bef23272b
Alexios Karadimos <karadalex@gmail.com>
2022-01-16 19:07:11 +0200

    Compress images by reducing their colorspaces

--- reduce.sh
+++ reduce.sh
@@ -1,2 +1,2 @@
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -dPrinted=false -sOutputFile=thesis-karadimos-2020-300dpi.pdf thesis-karadimos-2020.pdf
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -dPrinted=false -sOutputFile=thesis-karadimos-2020-150dpi.pdf thesis-karadimos-2020.pdf
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -dPrinted=false -sOutputFile=thesis-karadimos-2022-300dpi.pdf thesis-karadimos-2022.pdf
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -dPrinted=false -sOutputFile=thesis-karadimos-2022-150dpi.pdf thesis-karadimos-2022.pdf
